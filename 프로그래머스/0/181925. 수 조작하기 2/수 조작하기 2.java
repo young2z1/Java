6076fe4c4370261c0c2418b508f18c1746ff40a1
young2z1 <fftizlk0@gmail.com>
2026-02-06 23:04:26 +0900

    [level 0] Title: 수 조작하기 2, Time: 1100.13 ms, Memory: 381 MB -BaekjoonHub

---
+++ 프로그래머스/0/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        
+        for (int i = 1; i<numLog.length; i++) {
+            if (numLog[i-1]+1 == numLog[i]) {
+                answer += "w";
+            } else if (numLog[i-1]-1 == numLog[i]) {
+                answer += "s";
+            } else if (numLog[i-1]+10 == numLog[i]) {
+                answer += "d";
+            } else if (numLog[i-1]-10 == numLog[i]) {
+                answer += "a";
+            }
+        }
+        
+        return answer;
+    }
+}
