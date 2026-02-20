14b6ac2c810cfb50e648a735e0f6cf2de151835e
young2z1 <fftizlk0@gmail.com>
2026-02-20 23:13:56 +0900

    [level 0] Title: 글자 지우기, Time: 0.57 ms, Memory: 85.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181900. 글자 지우기/글자 지우기.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String my_string, int[] indices) {
+        StringBuilder answer = new StringBuilder(my_string);
+        Arrays.sort(indices);
+        
+        for (int i=indices.length-1; i>=0; i--) {
+            answer.deleteCharAt(indices[i]);
+        }
+        
+        return answer.toString();
+    }
+}
