488481754d3b88117a223508122b35ebe0e57801
young2z1 <fftizlk0@gmail.com>
2026-03-01 11:09:01 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.03 ms, Memory: 88.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String[] str_list) {
+        String[] answer = new String[0];
+        
+        for (int i = 0; i<str_list.length; i++) {
+            if (str_list[i].equals("l")) {
+                answer = Arrays.copyOfRange(str_list, 0, i);
+                break;
+            } else if (str_list[i].equals("r")) {
+                answer = Arrays.copyOfRange(str_list, i+1, str_list.length);
+                break;
+            } else {
+                answer = new String[0];
+            }
+        }
+        
+        return answer;
+    }
+}
