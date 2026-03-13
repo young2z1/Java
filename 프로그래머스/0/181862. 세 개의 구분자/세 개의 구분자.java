042f0c136fdd9392eac56e0b4dff42f507214f18
young2z1 <fftizlk0@gmail.com>
2026-03-13 19:02:53 +0900

    [level 0] Title: 세 개의 구분자, Time: 44.82 ms, Memory: 101 MB -BaekjoonHub

--- 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
+++ 프로그래머스/0/181862. 세 개의 구분자/세 개의 구분자.java
@@ -3,24 +3,14 @@ import java.util.ArrayList;
 
 class Solution {
     public String[] solution(String myStr) {
-        myStr = myStr.replaceAll("[a-c]", " ");
+        myStr = myStr.replaceAll("[a-c]+", " ").trim();
         String[] em = {"EMPTY"};
         String[] st = myStr.split(" ");
-        ArrayList<String> al = new ArrayList<>(Arrays.asList(st));
-        for (int i = 0; i<al.size(); i++) {
-            if (al.get(i).isEmpty()) {
-                al.remove(i);
-                i--;
-            }
-        }
-        String[] answer = new String[al.size()];
-        for (int i = 0; i<al.size(); i++) {
-            answer[i] = al.get(i);
-        }
-        if (st.length == 0) {
+        
+        if (st[0].equals("")) {
             return em;
         } else {
-            return answer;
+            return st;
         }
     }
 }
