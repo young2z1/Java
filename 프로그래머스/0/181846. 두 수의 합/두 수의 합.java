e78b6d374f9ed9bfcdbb354fee8d830feb2277e2
young2z1 <fftizlk0@gmail.com>
2026-03-25 06:28:31 +0900

    [level 0] Title: 두 수의 합, Time: 417.61 ms, Memory: 104 MB -BaekjoonHub

---
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+class Solution {
+    public String solution(String a, String b) {
+        BigInteger ia = new BigInteger(a);
+        BigInteger ib= new BigInteger(b);
+        return ia.add(ib).toString();
+        
+    }
+}
