b7124aed8cd38e4013f5ded62739b59cbe51ccfd
young2z1 <fftizlk0@gmail.com>
2026-03-29 12:20:31 +0900

    [level 0] Title: 정수 찾기, Time: 0.03 ms, Memory: 75 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int solution(int[] num_list, int n) {
+        int answer = -1;
+        for (int i : num_list) {
+            if (i == n) {
+                answer = 1;
+                break;
+            }
+            else answer = 0;
+        }
+        return answer;
+    }
+}
